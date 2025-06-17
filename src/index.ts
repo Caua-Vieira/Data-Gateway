5cea8eea717cf7b47fdc72a9db11ffbdecf2629a
Caua-Vieira <cauasilva8@gmail.com>
2025-06-17 19:38:26 -0300

    feat: estrutura inicial do projeto DataGateway

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import './infrastructure/config/ioc';
+import app from './interfaces/server';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
