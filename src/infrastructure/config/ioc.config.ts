057e1fe1f8755cc471b06549256804c61642fc1e
Caua-Vieira <cauasilva8@gmail.com>
2025-06-24 20:04:32 -0300

    refactor: modificar caminho para repository

--- src/infrastructure/config/ioc.config.ts
+++ src/infrastructure/config/ioc.config.ts
@@ -2,7 +2,7 @@ import { Container } from "typescript-ioc";
 import { AbstractPayloadValidator } from "../../domain/contracts/abstractPayloadValidator";
 import { ZodPayloadValidator } from "../validators/zodPayloadValidator";
 import { AbstractPayloadRepository } from "../../domain/contracts/abstractPayloadRepository";
-import { PayloadRepository } from "../repository/payloadRepository";
+import { PayloadRepository } from "../repositories/payloadRepository";
 
 Container.bind(AbstractPayloadValidator).to(ZodPayloadValidator);
 Container.bind(AbstractPayloadRepository).to(PayloadRepository);
